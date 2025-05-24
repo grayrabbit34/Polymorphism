10da2bc82e4d9c6279d26fec1478660bd31cf4ab
BURAK DÜLGEROĞLU <dulgerogluburak04@gmail.com>
2025-05-24 23:52:12 +0700

    Polymorphism

---
+++ Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Polymorphism
+{
+    public class BaseGeometrikSekil
+    {
+
+        private double _width; //fields
+
+        private double _height;
+
+        public double Width //property for Width
+        {
+            get { return _width; }
+
+            set // set Width a value only if its positive number
+            {
+                if (value > 0)
+                    _width = value;
+                else
+                {
+                    Console.WriteLine("Pozitif bir sayı giriniz.");
+                }
+            }
+        }
+
+
+        public double Height
+        {
+            get { return _height; }
+
+            set
+            {
+                if (value > 0)
+                    _height = value;
+                else
+                    Console.WriteLine("Pozitif bir sayı giriniz.");
+            }
+        }
+
+        public virtual void AlanHesapla()
+        {
+            double alan = Width * Height;
+            Console.WriteLine($"Şeklin alanı: {alan}");
+        }
+    }
+
+    public class Square : BaseGeometrikSekil
+    {
+
+    }
+
+    public class Rectangle : BaseGeometrikSekil
+    {
+
+    }
+
+
+    public class RightTriangle : BaseGeometrikSekil
+    {
+        public override void AlanHesapla()
+        {
+            double alan = Width * Height / 2;
+            Console.WriteLine($"Şeklin alanı: {alan}");
+        }
+
+    }
+}
+
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,30 @@
+﻿using Polymorphism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Square square = new Square();
+
+        square.Width = 23.5;
+        square.Height = 34.67;
+
+        square.AlanHesapla();
+
+
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.Height = 5;
+        rectangle.Width = 15;
+
+        rectangle.AlanHesapla();
+
+        RightTriangle rightTriangle = new RightTriangle();
+
+        rightTriangle.Height = 5;
+        rightTriangle.Width = 45;
+
+        rightTriangle.AlanHesapla();
+    }
+
+}
